9f11fbf4c0c1a41e247e5b8329c250e4478ac6af
Elizabeth Hunke <eclare@lanl.gov>
2016-07-29 20:47:56 +0000

    trunk: Merge r1132:1133 from rasm_colpkg branch - use iceumask instead of icetmask for dynamics halo masking, to fix occasional exact restart problem and rare and intermittent error in halo update in the dynamics.  BFB in one-year tests in the standard gx3 configuration. Thanks to Tony Craig for this.

--- source/ice_dyn_evp.F90
+++ source/ice_dyn_evp.F90
@@ -313,6 +313,7 @@
       ! velocities may have changed in evp_prep2
       call ice_HaloUpdate (fld2,               halo_info, &
                            field_loc_NEcorner, field_type_vector)
+      call ice_timer_stop(timer_bound)
 
       ! unload
       !$OMP PARALLEL DO PRIVATE(iblk)
@@ -322,9 +323,15 @@
       enddo
       !$OMP END PARALLEL DO
 
-      if (maskhalo_dyn) &
-         call ice_HaloMask(halo_info_mask, halo_info, icetmask)
-      call ice_timer_stop(timer_bound)
+      if (maskhalo_dyn) then
+         call ice_timer_start(timer_bound)
+         halomask = 0
+         where (iceumask) halomask = 1
+         call ice_HaloUpdate (halomask,          halo_info, &
+                              field_loc_center,  field_type_scalar)
+         call ice_timer_stop(timer_bound)
+         call ice_HaloMask(halo_info_mask, halo_info, halomask)
+      endif
 
       do ksub = 1,ndte        ! subcycling
 
@@ -390,6 +397,7 @@
             call ice_HaloUpdate (fld2,               halo_info, &
                                  field_loc_NEcorner, field_type_vector)
          endif
+         call ice_timer_stop(timer_bound)
 
          ! unload
          !$OMP PARALLEL DO PRIVATE(iblk)
@@ -398,7 +406,6 @@
             vvel(:,:,iblk) = fld2(:,:,2,iblk)
          enddo
          !$OMP END PARALLEL DO
-         call ice_timer_stop(timer_bound)
 
       enddo                     ! subcycling
 
