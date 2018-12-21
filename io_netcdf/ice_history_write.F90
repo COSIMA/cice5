3f755d3c7d5a183a1ee2480b0b9dc7788020018b
Aidan Heerdegen <aidan.heerdegen@anu.edu.au>
2018-12-21 15:59:46 +1100

    Changed netcdf creation mode to NF90_CLASSIC_MODEL. This is backwards compatible, and so is better supported by a wider range of software.
    There are no new features currently being used that necessitate using
    NF90_HDF5, so there should be only upsides and no downsides to this change.

--- io_netcdf/ice_history_write.F90
+++ io_netcdf/ice_history_write.F90
@@ -127,7 +127,7 @@
         endif
 
         ! create file
-        status = nf90_create(ncfile(ns), NF90_HDF5, ncid)
+        status = nf90_create(ncfile(ns), NF90_CLASSIC_MODEL, ncid)
         if (status /= nf90_noerr) call abort_ice( &
            'ice: Error creating history ncfile '//ncfile(ns))
 
