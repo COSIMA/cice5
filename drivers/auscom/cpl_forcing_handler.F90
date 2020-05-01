7dee17fa09574cf8f2853d05a2d0da6ce0536a16
Russell Fiedler <raf599@gadi-login-02.gadi.nci.org.au>
2020-05-01 16:41:14 +1000

    wnd10_10 written to i2o,nc

--- drivers/auscom/cpl_forcing_handler.F90
+++ drivers/auscom/cpl_forcing_handler.F90
@@ -207,6 +207,7 @@ implicit none
         call ice_open_nc(fname, ncid_i2o) 
         do i=1, num_fields_to_ocn
             vwork(:, :, :) = 0.0
+      if (my_task == master_task) write(il_out,*) '(get_time0_i2o_fields) reading', trim(fields_to_ocn(i)
             call ice_read_nc(ncid_i2o, 1, trim(fields_to_ocn(i)) , vwork, .true.)
 
             if (trim(fields_to_ocn(i)) == 'strsu_io') then
@@ -427,6 +428,8 @@ implicit none
             vwork = iolicefw
         elseif (trim(fields_to_ocn(i)) == 'licefh_io') then
             vwork = iolicefh
+        elseif (trim(fields_to_ocn(i)) == 'wnd10_io') then
+            vwork = iownd
         else
             call abort_ice('ice: bad save array name '//trim(fields_to_ocn(i)))
         endif
